33939ee6f7d8fec31bcab789ee52c61b0e5c238a
sttbabc <131928835+sttbabc@users.noreply.github.com>
2024-05-05 14:48:37 -0500

    Update cdk_static_website.py
    reference added

--- cdk_static_website.py
+++ cdk_static_website.py
@@ -2,6 +2,7 @@ from constructs import Construct
 from cdktf import App, TerraformStack
 from imports.aws import AwsProvider, S3Bucket
 
+# example useage from Teraform CDK gitrepo https://github.com/hashicorp/terraform-cdk/blob/main/examples/python/aws/main.py
 class MyStaticWebsiteStack(TerraformStack):
     def __init__(self, scope: Construct, name: str):
         super().__init__(scope, name)
