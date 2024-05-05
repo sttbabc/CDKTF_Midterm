02405db9743d3f85404be558ef11c3b8c627aa1f
sttbabc <131928835+sttbabc@users.noreply.github.com>
2024-05-05 14:45:11 -0500

    Add files via upload
    basic html and py script for use with CDKTF

---
+++ cdk_static_website.py
@@ -0,0 +1,22 @@
+from constructs import Construct
+from cdktf import App, TerraformStack
+from imports.aws import AwsProvider, S3Bucket
+
+class MyStaticWebsiteStack(TerraformStack):
+    def __init__(self, scope: Construct, name: str):
+        super().__init__(scope, name)
+
+        # AWS 
+        AwsProvider(self, 'aws', region='us-west-2')
+
+        # S3 bucket 
+        website_bucket = S3Bucket(self, 'MyStaticWebsiteBucket',
+                                  bucket='my-static-website-bucket',
+                                  acl='public-read',
+                                  website_index_document='index.html',
+                                  website_error_document='error.html')
+
+app = App()
+MyStaticWebsiteStack(app, "my-static-website-stack")
+
+app.synth()
